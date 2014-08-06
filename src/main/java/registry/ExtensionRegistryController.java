17d560f6abc56b3e9e95e42747fc91f1058f658f
jenf11 <jenfry11@gmail.com>
2014-08-06 14:54:51 +0200

    added error messages

--- src/main/java/registry/ExtensionRegistryController.java
+++ src/main/java/registry/ExtensionRegistryController.java
@@ -19,7 +19,10 @@
  */
 package registry;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.commons.io.IOUtils;
 import org.apache.felix.ipojo.annotations.Requires;
 import org.wisdom.api.DefaultController;
@@ -32,9 +35,11 @@ import org.wisdom.api.templates.Template;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.Callable;
 
 /**
  * Your first Wisdom Controller.
@@ -119,42 +124,71 @@ public class ExtensionRegistryController extends DefaultController {
 
     @Route(method = HttpMethod.DELETE, uri = "/list/{id}")
     public Result delete(@Parameter("id") String id) {
-        System.out.println("deleting "+id);
         removeExtensionById(id);
         return ok();
     }
 
     @Route(method = HttpMethod.POST, uri = "/upload")
-    public Result findExt(@FormParameter("url") String u) throws IOException {
-        System.out.println("we made it here - " + u);
-       URL url = new URL(u);
-       String j = IOUtils.toString(url);
-       JsonNode node = json.parse(j);
-        addToList(node);
-        return ok(node);
+    public Result findExt(final @FormParameter("url") String u) {
 
-     }
+        return async(new Callable<Result>() {
+            @Override
+            public Result call() throws Exception {
+                try {
+                    URL url = new URL(u);
+                    String j = IOUtils.toString(url);
+                    JsonNode node = json.parse(j);
+                    node = addToList(node);
+                    return ok(node);
+                } catch (IOException e) {
+                    // Cannot read the URL
+                    logger().error("File not found or is unable to be read " + e );
+
+                    return ok(json.newObject().put("error","An error has occurred:").put("reason",
+                            "File not found or unable to be read."));
+                } catch (Exception e) {
+                    logger().error(" A problem has occurred please check that your file is " +
+                            "valid JSON. " +
+                            " - " + e.getMessage() +
+                            " (" + e.getClass()
+                            .getName() + ")");
+                    return ok(json.newObject().put("error","An error has occurred:").put("reason",
+                            "Please check that your file is " +
+                            "valid JSON. " ));
+                }
+            }
+        });
+
+
+    }
 
     @Requires
     Validator validator;
 
-    private void addToList(JsonNode node){
-        //Todo needs error checking, and how to parse objects and array? what to return?
-        System.out.println("add to list");
+    private JsonNode addToList(JsonNode node){
+        ObjectNode result =  json.newObject();
         Extension extension = json.mapper().convertValue(node, Extension.class);
         Set<ConstraintViolation<Extension>> errors = validator.validate(extension);
         if (! errors.isEmpty()) {
+            Iterator<ConstraintViolation<Extension>> i = errors.iterator();
+            String msg = "";
+             while(i.hasNext()){
+                 msg = msg  + i.next().getPropertyPath()+ " ";
+
+             }
+            result.put("error", "Error while adding extension").put("reason",
+                    msg + "need(s) to be set");
             logger().error("Something really bad happened...");
             logger().error(errors.toString());
-            return;
+            return result;
         }
 
         extension.setDate(new SimpleDateFormat("dd-MM-yyyy").format(new Date()));
         list.put(extension.getName(), extension);
+        return node;
     }
     private void removeExtensionById(String id) {
         list.remove(id);
-        System.out.println(list.keySet());
     }
 
 }
